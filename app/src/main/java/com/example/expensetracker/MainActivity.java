cce86323a96bf76bc3f13e43e2856f56a9e368e0
abhishek jain <abhishekjain00e@gmail.com>
2025-05-01 23:11:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.expensetracker;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.expensetracker.ui.home.ExpenseFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.expensetracker.R*;
+import com.example.expensetracker.ui.home.ListFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        // Load default fragment
+        loadFragment(new ExpenseFragment());
+
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.:
+                    selectedFragment = new ExpenseFragment();
+                    break;
+                case R.id.nav_list:
+                    selectedFragment = new ListFragment();
+                    break;
+            }
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/example/expensetracker/ui/splashScreen.java
@@ -0,0 +1,44 @@
+package com.example.expensetracker.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.expensetracker.MainActivity;
+import com.example.expensetracker.R;
+
+import java.util.Objects;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Thread thread = new Thread() {
+            public void run(){
+                try {
+                    sleep(1500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(splashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };thread.start();
+    }
+}
